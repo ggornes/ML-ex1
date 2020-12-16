ecb2bfe6af1d6c47c50119a446ffe40946581719
ggornes <gerardogornes@gmail.com>
2020-12-16 15:38:19 +0800

    added code as assignment instructions

--- ex1/ex1.m
+++ ex1/ex1.m
@@ -40,7 +40,7 @@ pause;
 fprintf('Plotting Data ...\n')
 data = load('ex1data1.txt');
 X = data(:, 1); y = data(:, 2);
-m = length(y); % number of training examples
+m = length(y) % number of training examples
 
 % Plot Data
 % Note: You have to complete the code in plotData.m
