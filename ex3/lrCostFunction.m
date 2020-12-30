f2cda021b70624dd3a075f06b09ebb906d898fa5
ggornes <gerardogornes@gmail.com>
2020-12-30 10:43:31 +0800

    calculated cost function and gradient vector

--- ex3/lrCostFunction.m
+++ ex3/lrCostFunction.m
@@ -39,7 +39,8 @@ grad = zeros(size(theta));
 
 J = (1/m) * (-y' * log(sigmoid(X * theta)) - (1 - y)' * log(1 - sigmoid((X * theta))) ) + (lambda/(2*m)) * sum(theta(2:end) .* theta(2:end));
 
-grad = (1/m) * X' * (sigmoid(X * theta) - y) + (lambda/m)*[0; theta(2:end)];
+grad = (1/m) * X' * (sigmoid(X * theta) - y); % unregularized gradient
+grad = grad + (lambda/m)*[0; theta(2:end)]; % regularized gradient
 
 
 
