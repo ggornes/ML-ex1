c30c71d6b351cc8dee665cba565181b3002ecdfb
ggornes <gerardogornes@gmail.com>
2021-01-18 10:27:51 +0800

    finished ex4

--- ex4/nnCostFunction.m
+++ ex4/nnCostFunction.m
@@ -41,6 +41,23 @@ Theta2_grad = zeros(size(Theta2)); % 10x26
 
 X = [ones(m, 1) X];
 % A = ones(size(X,1), size(X,2))';
+
+a1 = zeros(size(X)); % 5000x401
+z2 = zeros(m, hidden_layer_size); % 5000x25
+a2 = zeros(m, hidden_layer_size + 1); % 5000x26
+a3 = zeros(m, num_labels); % 5000x10
+d3 = zeros(m, num_labels); % 5000x10
+d2 = zeros(m, hidden_layer_size); % 5000x25
+
+
+% a1 = X;
+% a1 = [ones(5000, 1) a1];
+% a2 = Theta1 * a1';
+% a2 = a2';
+% a2 = [ones(5000, 1) a2];
+% z3 = Theta2*a2';
+% a3 = sigmoid(z3);
+
 yv=[1:num_labels] == y;
 
 sum1 = 0;
@@ -52,15 +69,16 @@ reg_theta2 = 0;
 
 for i = 1 : m
     
-    %% A1 = [ones(m, 1) X];
     A1 = X(i,:); % 1x401
+    a1(i,:) = A1;
     Z2 = Theta1 * A1'; % 25x1
     A2 = sigmoid(Z2); % 25x1
     A2 = A2'; % 1x25
-    %% A2 = [ones(m, 1) A2];
     A2 = [ones(size(A2, 1), 1) A2]; % 1x26
+    a2(i,:) = A2;
     Z3 = Theta2*A2'; % 10x1
     A3 = sigmoid(Z3); % 10x1
+    a3(i,:) = A3;
 
     sum2 = 0;
     for k = 1 : num_labels
@@ -118,6 +136,39 @@ J = -(1/m) * sum1 + (lambda/(2*m))*reg;
 %         Hint: We recommend implementing backpropagation using a for-loop
 %               over the training examples if you are implementing it for the 
 %               first time.
+
+d3 = a3 - yv;
+d2 = (d3*Theta2(:,2:end)) .* a2(:,2:end) .* (1 - a2(:,2:end)); % 26x26
+
+Delta1 = d2'*a1;
+Delta2 = d3'*a2;
+
+Theta1_grad = (1/m) * Delta1;
+Theta2_grad = (1/m) * Delta2;
+
+
+for i = 1 : size(Delta1, 1)
+    for j = 2 : size(Delta1, 2)
+        if (j != 0)
+            Theta1_grad(i,j) = Theta1_grad(i,j) + (lambda/m) * Theta1(i,j);
+        end
+    end
+end
+
+for i = 1 : size(Delta2, 1)
+    for j = 2 : size(Delta2, 2)
+        if (j != 0)
+            Theta2_grad(i,j) = Theta2_grad(i,j) + (lambda/m) * Theta2(i,j);
+        end
+    end
+end
+
+
+grad = [Theta1_grad(:) ; Theta2_grad(:)];
+
+
+
+
 %
 % Part 3: Implement regularization with the cost function and gradients.
 %
